6b1b174f6bf523dc98f42dcf44243b50f6f1dc95
gamer <paola33novios@hotmail.com>
2025-05-01 17:57:25 -0600

    Add files via upload

---
+++ js/registro.js
@@ -0,0 +1,440 @@
+ window.F2W_REACTIONS = (() => {
+    const t = [
+        [{
+            key: "background-color",
+            from: "rgba(255,255,255,0.63)",
+            to: "#f17102"
+        }, {
+            key: "filter",
+            from: "drop-shadow(0px 4px 4px rgba(0,0,0,0.25))",
+            to: "revert"
+        }, {
+            key: "backface-visibility",
+            from: "hidden",
+            to: "revert"
+        }, {
+            key: "transform",
+            from: "translateZ(0)",
+            to: "revert"
+        }, {
+            key: "box-shadow",
+            from: "revert",
+            to: "0px 0px 29.8px #0109f4,0px 0px 17px #0109f4,0px 0px 9.9px #0109f4,0px 0px 5px #0109f4,0px 0px 1.4px #0109f4,0px 0px 0.7px #0109f4"
+        }],
+        [{
+            key: "font-family",
+            from: "Arial",
+            to: '"Arial Rounded MT Bold"'
+        }, {
+            key: "color",
+            from: "#767373",
+            to: "#fff"
+        }],
+        [{
+            key: "border-width",
+            from: "0px 0px 1px",
+            to: "0px 0px 2px"
+        }, {
+            key: "border-color",
+            from: "#767373",
+            to: "#fff"
+        }],
+        [{
+            key: "fill",
+            from: "rgba(64,61,61,0.63)",
+            to: "#fff"
+        }],
+        [{
+            key: "stroke",
+            from: "rgba(64,61,61,0.63)",
+            to: "#fff"
+        }],
+        [{
+            key: "background-color",
+            from: "rgba(255,255,255,0.63)",
+            to: "#f17102"
+        }, {
+            key: "filter",
+            from: "drop-shadow(0px 4px 4px rgba(0,0,0,0.25))",
+            to: "revert"
+        }, {
+            key: "backface-visibility",
+            from: "hidden",
+            to: "revert"
+        }, {
+            key: "transform",
+            from: "translateZ(0)",
+            to: "revert"
+        }, {
+            key: "box-shadow",
+            from: "revert",
+            to: "0px 0px 64.6px #0109f4,0px 0px 36.9px #0109f4,0px 0px 21.5px #0109f4,0px 0px 10.8px #0109f4,0px 0px 3.1px #0109f4,0px 0px 1.5px #0109f4"
+        }],
+        [{
+            key: "background-color",
+            from: "#4e23e7",
+            to: "#2601ab"
+        }, {
+            key: "box-shadow",
+            from: "revert",
+            to: "0px 0px 29.8px #4e23e7,0px 0px 17px #4e23e7,0px 0px 9.9px #4e23e7,0px 0px 5px #4e23e7,0px 0px 1.4px #4e23e7,0px 0px 0.7px #4e23e7"
+        }],
+        [{
+            key: "top",
+            from: "calc(-8.5px + 50%)",
+            to: "calc(-9px + 50%)"
+        }],
+        [{
+            key: "top",
+            from: "calc(-15px + 50%)",
+            to: "calc(-13px + 50%)"
+        }],
+        [{
+            key: "background-color",
+            from: "#f17102",
+            to: "#ba5904"
+        }, {
+            key: "box-shadow",
+            from: "revert",
+            to: "0px 0px 29.8px #f17102,0px 0px 17px #f17102,0px 0px 9.9px #f17102,0px 0px 5px #f17102,0px 0px 1.4px #f17102,0px 0px 0.7px #f17102"
+        }],
+        [{
+            key: "top",
+            from: "calc(-8.5px + 50%)",
+            to: "calc(-9px + 50%)"
+        }, {
+            key: "width",
+            from: "127px",
+            to: "max-content"
+        }]
+    ];
+    return {
+        433: {
+            type: "ANIMATE",
+            transition: {
+                type: "SMART_ANIMATE",
+                easing: "linear(0, 0.065 2.7%, 0.778 13.5%, 0.914, 1.011, 1.071, 1.101, 1.108, 1.1 29.7%, 1.022 40.5%, 1.008 43.2%, 0.992, 0.988 54.1%, 1.001 75.7%, 1)",
+                duration: .6666666666666666
+            },
+            animations: [{
+                props: t[0],
+                reactions: [{
+                    type: "hover",
+                    from: "433"
+                }],
+                eltId: "_li_2"
+            }, {
+                props: t[1],
+                eltId: "_label_2"
+            }, {
+                props: t[1],
+                eltId: "__5"
+            }, {
+                props: t[2],
+                eltId: "_input_"
+            }, {
+                props: t[3],
+                eltId: "Vector_2_0"
+            }],
+            rootId: "_li_2"
+        },
+        448: {
+            type: "ANIMATE",
+            transition: {
+                type: "SMART_ANIMATE",
+                easing: "linear(0, 0.065 2.7%, 0.778 13.5%, 0.914, 1.011, 1.071, 1.101, 1.108, 1.1 29.7%, 1.022 40.5%, 1.008 43.2%, 0.992, 0.988 54.1%, 1.001 75.7%, 1)",
+                duration: .6666666666666666
+            },
+            animations: [{
+                props: t[0],
+                reactions: [{
+                    type: "hover",
+                    from: "448"
+                }],
+                eltId: "_li_3"
+            }, {
+                props: t[1],
+                eltId: "_label_3"
+            }, {
+                props: t[1],
+                eltId: "__6"
+            }, {
+                props: t[2],
+                eltId: "_input_2"
+            }, {
+                props: t[3],
+                eltId: "Vector_3_0"
+            }],
+            rootId: "_li_3"
+        },
+        463: {
+            type: "ANIMATE",
+            transition: {
+                type: "SMART_ANIMATE",
+                easing: "linear(0, 0.065 2.7%, 0.778 13.5%, 0.914, 1.011, 1.071, 1.101, 1.108, 1.1 29.7%, 1.022 40.5%, 1.008 43.2%, 0.992, 0.988 54.1%, 1.001 75.7%, 1)",
+                duration: .6666666666666666
+            },
+            animations: [{
+                props: t[0],
+                reactions: [{
+                    type: "hover",
+                    from: "463"
+                }],
+                eltId: "_li_4"
+            }, {
+                props: t[1],
+                eltId: "_label_4"
+            }, {
+                props: t[1],
+                eltId: "__7"
+            }, {
+                props: t[2],
+                eltId: "_input_3"
+            }, {
+                props: t[4],
+                eltId: "Vector_4"
+            }, {
+                props: t[4],
+                eltId: "Vector_5"
+            }, {
+                props: t[4],
+                eltId: "Vector_6"
+            }],
+            rootId: "_li_4"
+        },
+        480: {
+            type: "ANIMATE",
+            transition: {
+                type: "SMART_ANIMATE",
+                easing: "linear(0, 0.065 2.7%, 0.778 13.5%, 0.914, 1.011, 1.071, 1.101, 1.108, 1.1 29.7%, 1.022 40.5%, 1.008 43.2%, 0.992, 0.988 54.1%, 1.001 75.7%, 1)",
+                duration: .6666666666666666
+            },
+            animations: [{
+                props: t[5],
+                reactions: [{
+                    type: "hover",
+                    from: "480"
+                }],
+                eltId: "_li_5"
+            }, {
+                props: t[1],
+                eltId: "_label_5"
+            }, {
+                props: t[1],
+                eltId: "__8"
+            }, {
+                props: t[2],
+                eltId: "_input_4"
+            }, {
+                props: t[3],
+                eltId: "Vector_7_0"
+            }],
+            rootId: "_li_5"
+        },
+        495: {
+            type: "ANIMATE",
+            transition: {
+                type: "SMART_ANIMATE",
+                easing: "linear(0, 0.065 2.7%, 0.778 13.5%, 0.914, 1.011, 1.071, 1.101, 1.108, 1.1 29.7%, 1.022 40.5%, 1.008 43.2%, 0.992, 0.988 54.1%, 1.001 75.7%, 1)",
+                duration: .6666666666666666
+            },
+            animations: [{
+                props: t[0],
+                reactions: [{
+                    type: "hover",
+                    from: "495"
+                }],
+                eltId: "_li_6"
+            }, {
+                props: t[1],
+                eltId: "_label_6"
+            }, {
+                props: t[1],
+                eltId: "__9"
+            }, {
+                props: t[2],
+                eltId: "_input_5"
+            }, {
+                props: t[3],
+                eltId: "Vector_8_0"
+            }],
+            rootId: "_li_6"
+        },
+        510: {
+            type: "ANIMATE",
+            transition: {
+                type: "SMART_ANIMATE",
+                easing: "linear(0, 0.065 2.7%, 0.778 13.5%, 0.914, 1.011, 1.071, 1.101, 1.108, 1.1 29.7%, 1.022 40.5%, 1.008 43.2%, 0.992, 0.988 54.1%, 1.001 75.7%, 1)",
+                duration: .6666666666666666
+            },
+            animations: [{
+                props: t[0],
+                reactions: [{
+                    type: "hover",
+                    from: "510"
+                }],
+                eltId: "_li_7"
+            }, {
+                props: t[1],
+                eltId: "_label_7"
+            }, {
+                props: t[1],
+                eltId: "__10"
+            }, {
+                props: t[2],
+                eltId: "_input_6"
+            }, {
+                props: t[3],
+                eltId: "Vector_9_0"
+            }],
+            rootId: "_li_7"
+        },
+        525: {
+            type: "ANIMATE",
+            transition: {
+                type: "SMART_ANIMATE",
+                easing: "linear(0, 0.065 2.7%, 0.778 13.5%, 0.914, 1.011, 1.071, 1.101, 1.108, 1.1 29.7%, 1.022 40.5%, 1.008 43.2%, 0.992, 0.988 54.1%, 1.001 75.7%, 1)",
+                duration: .6666666666666666
+            },
+            animations: [{
+                props: t[6],
+                reactions: [{
+                    type: "hover",
+                    from: "525"
+                }],
+                eltId: "button-responsive"
+            }, {
+                props: t[7],
+                eltId: "I127_156_125_276"
+            }, {
+                props: t[8],
+                eltId: "mdi_register"
+            }],
+            rootId: "button-responsive"
+        },
+        534: {
+            type: "ANIMATE",
+            transition: {
+                type: "SMART_ANIMATE",
+                easing: "linear(0, 0.065 2.7%, 0.778 13.5%, 0.914, 1.011, 1.071, 1.101, 1.108, 1.1 29.7%, 1.022 40.5%, 1.008 43.2%, 0.992, 0.988 54.1%, 1.001 75.7%, 1)",
+                duration: .6666666666666666
+            },
+            animations: [{
+                props: t[9],
+                reactions: [{
+                    type: "hover",
+                    from: "534"
+                }],
+                eltId: "button-responsive2"
+            }, {
+                props: t[10],
+                eltId: "I127_170_120_239"
+            }, {
+                props: t[8],
+                eltId: "stash_image_plus"
+            }],
+            rootId: "button-responsive2"
+        }
+    }
+})(), window.F2W_VARIABLES = {
+    "--Breakpoints": 360,
+    "--Type-size-H1": 28,
+    "--Type-size-H2": 25,
+    "--Type-size-H3": 18,
+    "--Type-size-H4": 15,
+    "--Type-size-H5": 0,
+    "--Font-family-title": "arial",
+    "--Font-family-sub-title": "arial",
+    "--Font-family-paragraphs": "arial",
+    "--container": 296,
+    "--image-width": 153,
+    "--image-height": 60,
+    "--container-logo-width": 360,
+    "--container-logo-height": 70,
+    "--pading-left": 30,
+    "--pading-right": 30,
+    "--button-width": 296,
+    "--button-height": 36,
+    "--container-button-width": 296,
+    "--container-button-height": 96,
+    "--icon-width": 25,
+    "--icon-height": 25,
+    "--block-text-width": 296,
+    "--block-text-height": 76,
+    "--Type-size-display": 25,
+    "--container-form-padding-left-right": 50,
+    "--content-icon-width": 25,
+    "--content-icon-height": 25
+}, window.F2W_COLLECTION_VARS = {
+    responsive: {
+        desktop: {
+            "--Breakpoints": 1440,
+            "--Type-size-H1": 37,
+            "--Type-size-H2": 28,
+            "--Type-size-H3": 21,
+            "--Type-size-H4": 19,
+            "--Type-size-H5": 14,
+            "--Font-family-title": "Arial Rounded MT Bold",
+            "--Font-family-sub-title": "Arial Rounded MT Bold",
+            "--Font-family-paragraphs": "Arial Rounded MT Bold",
+            "--container": 671,
+            "--image-width": 257,
+            "--image-height": 101,
+            "--container-logo-width": 1440,
+            "--container-logo-height": 111,
+            "--pading-left": 60,
+            "--pading-right": 543,
+            "--button-width": 266,
+            "--button-height": 40,
+            "--container-button-width": 611,
+            "--container-button-height": 38,
+            "--icon-width": 29,
+            "--icon-height": 29,
+            "--block-text-width": 646,
+            "--block-text-height": 74,
+            "--Type-size-display": 48,
+            "--container-form-padding-left-right": 50,
+            "--content-icon-width": 40,
+            "--content-icon-height": 40
+        },
+        mobile: {
+            "--Breakpoints": 360,
+            "--Type-size-H1": 28,
+            "--Type-size-H2": 25,
+            "--Type-size-H3": 18,
+            "--Type-size-H4": 15,
+            "--Type-size-H5": 0,
+            "--Font-family-title": "arial",
+            "--Font-family-sub-title": "arial",
+            "--Font-family-paragraphs": "arial",
+            "--container": 296,
+            "--image-width": 153,
+            "--image-height": 60,
+            "--container-logo-width": 360,
+            "--container-logo-height": 70,
+            "--pading-left": 30,
+            "--pading-right": 30,
+            "--button-width": 296,
+            "--button-height": 36,
+            "--container-button-width": 296,
+            "--container-button-height": 96,
+            "--icon-width": 25,
+            "--icon-height": 25,
+            "--block-text-width": 296,
+            "--block-text-height": 76,
+            "--Type-size-display": 25,
+            "--container-form-padding-left-right": 50,
+            "--content-icon-width": 25,
+            "--content-icon-height": 25
+        }
+    }
+}, window.F2W_COLLECTION_MODE_BPS = {
+    responsive: {
+        mobile: {
+            minWidth: 0
+        },
+        desktop: {
+            minWidth: 1025
+        }
+    }
+}, window.F2W_COLOR_SCHEMES = void 0, window.F2W_LANGUAGES = void 0;
